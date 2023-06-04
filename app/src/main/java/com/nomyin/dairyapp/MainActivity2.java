e25f6b04525db1b882e7241d35397dbd6ca92123
NaomiInbal <nomyinbal@gmail.com>
2023-06-05 01:04:38 +0300

    list

--- app/src/main/java/com/nomyin/dairyapp/MainActivity2.java
+++ app/src/main/java/com/nomyin/dairyapp/MainActivity2.java
@@ -4,27 +4,40 @@ import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 //import android.content.DialogInterface;
+import android.app.DatePickerDialog;
 import android.content.ContentResolver;
 import android.content.DialogInterface;
 import android.content.Intent;
 //import android.os.Build;
 import android.database.Cursor;
+import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
+import android.provider.MediaStore;
+import android.text.InputType;
 import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
+import android.widget.DatePicker;
 import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.ListView;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import java.io.IOException;
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Calendar;
+import java.util.Date;
+import java.util.Locale;
 
 public class MainActivity2 extends AppCompatActivity {
     private Button btn_addEvent;
@@ -33,7 +46,19 @@ public class MainActivity2 extends AppCompatActivity {
     private ArrayAdapter<String> adapter;
     private ListView listView;
     private ArrayList<String> listData;
-
+    private static final int REQUEST_CONTACT_PICKER = 1;
+    private EditText eventNameInput;
+    private DatePicker eventDatePicker;
+    private Button chooseContactButton;
+    private Button eventDateInput;
+    private ContactsContract.Contacts selectedContactName;
+    private static final int REQUEST_IMAGE_PICKER  = 2;
+    private EditText eventNoteInput;
+    private ImageView eventImageView;
+    private Bitmap eventImageBitmap;
+    // STEP 1:
+    Intent takePhotoIntent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+    startActivityForResult(takePhotoIntent, REQUEST_CAMERA_PHOTO);
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -119,31 +144,173 @@ public class MainActivity2 extends AppCompatActivity {
             }
         });
     }
+
+
+//    private void showAddEventDialog() {
+//        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+//        builder.setTitle("Add New Event");
+//
+//        // Set up the input fields
+//        LinearLayout layout = new LinearLayout(this);
+//        layout.setOrientation(LinearLayout.VERTICAL);
+//        //name of the event
+//        eventNameInput = new EditText(this);
+//        //add contact
+//        eventNameInput.setHint("Enter event name");
+//
+//        Button chooseContactButton = new Button(this);
+//        chooseContactButton.setText("Choose Contact");
+//        chooseContactButton.setOnClickListener(new View.OnClickListener() {
+//            @Override
+//            public void onClick(View v) {
+//                Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+//                startActivityForResult(contactPickerIntent, REQUEST_CONTACT_PICKER);
+//            }
+//        });
+//
+//        layout.addView(eventNameInput);
+//        layout.addView(chooseContactButton);
+//
+//        // Set up the buttons
+//        builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
+//            @Override
+//            public void onClick(DialogInterface dialog, int which) {
+//                String eventName = eventNameInput.getText().toString();
+//                String contactName = selectedContactName != null ? selectedContactName : "";
+////                TextView textView = findViewById(R.id.tex);
+////                textView.setText(eventName + " - " + contactName);
+//
+//                events.add(new MyEvent(eventName));
+//                eventAdapter.notifyDataSetChanged();
+//            }
+//        });
+//
+//        builder.setNegativeButton("Cancel", null);
+//        builder.setView(layout);
+//
+//        // Create and show the dialog
+//        AlertDialog dialog = builder.create();
+//        dialog.setCanceledOnTouchOutside(false);
+//        dialog.show();
+//    }
+//
+//    @Override
+//    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+//        super.onActivityResult(requestCode, resultCode, data);
+//
+//        if (requestCode == REQUEST_CONTACT_PICKER && resultCode == RESULT_OK) {
+//            Uri contactUri = data.getData();
+//            String[] projection = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME};
+//            Cursor cursor = getContentResolver().query(contactUri, projection, null, null, null);
+//
+//            if (cursor != null && cursor.moveToFirst()) {
+//                int columnIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+//                selectedContactName = cursor.getString(columnIndex);
+//                cursor.close();
+//            }
+//        }
+//    }
+
     private void showAddEventDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Add New Event");
-        // Set up the input field
-        final EditText eventNameInput = new EditText(this);
+
+        // Set up the input fields
+        LinearLayout layout = new LinearLayout(this);
+        layout.setOrientation(LinearLayout.VERTICAL);
+
+        eventNameInput = new EditText(this);
         eventNameInput.setHint("Enter event name");
-        builder.setView(eventNameInput);
+
+        chooseContactButton = new Button(this);
+        chooseContactButton.setText("Choose a contact");
+        chooseContactButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent contactPickerIntent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+                startActivityForResult(contactPickerIntent, REQUEST_CONTACT_PICKER);
+
+            }
+        });
+
+        eventDateInput = new Button(this);
+        eventDateInput.setText("Choose a date");
+        eventDateInput.setInputType(InputType.TYPE_NULL);
+        eventDateInput.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // Create a Calendar instance
+                Calendar calendar = Calendar.getInstance();
+
+                // Get the current date values
+                int year = calendar.get(Calendar.YEAR);
+                int month = calendar.get(Calendar.MONTH);
+                int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+                // Create a date picker dialog
+                DatePickerDialog datePickerDialog = new DatePickerDialog(
+                        MainActivity2.this,
+                        (view, year1, monthOfYear, dayOfMonth) -> {
+                            // Update the event date input field with the selected date
+                            String selectedDate = year1 + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
+                            eventDateInput.setText(selectedDate);
+                        },
+                        year, month, day);
+
+                // Show the date picker dialog
+                datePickerDialog.show();
+            }
+        });
+
+        eventNoteInput = new EditText(this);
+        eventNoteInput.setHint("Notes:");
+
+        eventImageView = new ImageView(this);
+        eventImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent imagePickerIntent = new Intent(Intent.ACTION_PICK);
+                imagePickerIntent.setType("image/*");
+                startActivityForResult(Intent.createChooser(imagePickerIntent, "Select Image"), REQUEST_IMAGE_PICKER);
+            }
+        });
+
+        layout.addView(eventNameInput);
+        layout.addView(chooseContactButton);
+        layout.addView(eventDateInput);
+        layout.addView(eventNoteInput);
+        layout.addView(eventImageView);
 
         // Set up the buttons
         builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 String eventName = eventNameInput.getText().toString();
-                TextView textView = findViewById(R.id.tex);
-                textView.setText(eventName);
+                ContactsContract.Contacts selectedContact = selectedContactName;
+                ContactsContract.Contacts contactName = selectedContact;
+                String eventDateStr = eventDateInput.getText().toString();
+                String eventNote = eventNoteInput.getText().toString();
 
-                events.add(new MyEvent(eventName));
+                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+                Date eventDate = null;
+                try {
+                    eventDate = sdf.parse(eventDateStr);
+                } catch (ParseException e) {
+                    e.printStackTrace();
+                }
+
+                events.add(new MyEvent(eventName, contactName, eventDate, eventNote, eventImageBitmap));
+
                 eventAdapter.notifyDataSetChanged();
-
             }
         });
+
         builder.setNegativeButton("Cancel", null);
+        builder.setView(layout);
 
         // Create and show the dialog
         AlertDialog dialog = builder.create();
+        dialog.setCanceledOnTouchOutside(false);
         dialog.show();
     }
 
--- app/src/main/java/com/nomyin/dairyapp/MyEvent.java
+++ app/src/main/java/com/nomyin/dairyapp/MyEvent.java
@@ -1,10 +1,26 @@
 package com.nomyin.dairyapp;
 
+import android.graphics.Bitmap;
+import android.provider.ContactsContract;
+import android.util.Log;
+import android.widget.DatePicker;
+
+import java.util.Date;
+
 public class MyEvent {
     String eventName;
+    ContactsContract.Contacts contactName;
+    Date eventDate;
+    String eventNote;
+    Bitmap eventImageBitmap;
 //    Contact contact;
 
-    public MyEvent(String eventName) {
+    public MyEvent(String eventName, ContactsContract.Contacts contactName, Date eventDate, String eventNote, Bitmap eventImageBitmap) {
         this.eventName = eventName;
+        this.eventDate =eventDate;
+        this.contactName = contactName;
+        this.eventImageBitmap = eventImageBitmap;
+        this.eventNote = eventNote;
+        Log.d("mylog", "MyEvent: "+ eventName+ " "+ eventNote+" " + eventDate +" "+ contactName);
     }
 }
