6cc1ba260d79e3d4d3cc500b7860a58e98e1d1d6
NaomiInbal <nomyinbal@gmail.com>
2023-06-07 03:09:52 +0300

    Contact and date

--- app/src/main/java/com/nomyin/dairyapp/Contacts.java
+++ app/src/main/java/com/nomyin/dairyapp/Contacts.java
@@ -25,83 +25,79 @@ import java.util.ArrayList;
 public class Contacts extends AppCompatActivity {
 
     private ListView listView;
-        private ArrayList<String> listData;
-        private ArrayAdapter<String> adapter;
+    private ArrayList<String> listData;
+    private ArrayAdapter<String> adapter;
 
-        @Override
-        protected void onCreate(Bundle savedInstanceState)
-        {
-            super.onCreate(savedInstanceState);
-            setContentView(R.layout.activity_contacts);
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_contacts);
 
-            listView = findViewById(R.id.listID);
-            listData = new ArrayList<>();
-            adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
-            listView.setAdapter(adapter);
+        listView = findViewById(R.id.listID);
+        listData = new ArrayList<>();
+        adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1, listData);
+        listView.setAdapter(adapter);
 
-            listView.setOnItemClickListener(new AdapterView.OnItemClickListener()
-            {
-                @Override
-                public void onItemClick(AdapterView<?> parent, View view, int position, long id)
-                {
-                    Toast.makeText(Contacts.this, "onItemClick on pos: " +position, Toast.LENGTH_LONG).show();
-                }
-            });
-
-            listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener()
-            {
-                @Override
-                public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id)
-                {
-                    listData.remove(position);
-                    adapter.notifyDataSetChanged();
-                    return false;
-                }
-            });
-
-            Button btn = findViewById(R.id.btnLoadID);
-            btn.setOnClickListener(new View.OnClickListener()
-            {
-                @Override
-                public void onClick(View v)
-                {
-                    loadContacts();
-                }
-            });
-            loadContacts();
-        }
-
-        private void loadContacts()
-        {
-            listData.clear();
-
-            ContentResolver resolver = getContentResolver();
-
-            Uri contactsUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;  // contacts table uri
-            //String[] fields = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME};
-            //String sortOreder = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC"; // DESC(DOWN order) / ASC(UP order)
-            Cursor cursor = resolver.query(contactsUri,null,null,null, null); // SELECT * FROM Contacts
-
-            if(cursor != null && cursor.moveToFirst())
-            {
-                do
-                {
-                    int nameIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
-                    String name = cursor.getString(nameIndex);
-
-                    int phoneIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
-                    String num = cursor.getString(phoneIndex);
-
-                    Log.d("mylog", name + " " + num);
-                    listData.add(name + " " + num);
-                    adapter.notifyDataSetChanged();
-
-                } while (cursor.moveToNext());
-
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> parent, View view, int position, long id) {
+                Toast.makeText(Contacts.this, "onItemClick on pos: " + position, Toast.LENGTH_LONG).show();
             }
-            else
-            {
-                Toast.makeText(this, "EMPTY CONTACTS LIST", Toast.LENGTH_LONG).show();
+        });
+
+        listView.setOnItemLongClickListener(new AdapterView.OnItemLongClickListener() {
+            @Override
+            public boolean onItemLongClick(AdapterView<?> parent, View view, int position, long id) {
+                listData.remove(position);
+                adapter.notifyDataSetChanged();
+                return false;
             }
-        }
+        });
+
+//            Button btn = findViewById(R.id.btnLoadID);
+//            btn.setOnClickListener(new View.OnClickListener()
+//            {
+//                @Override
+//                public void onClick(View v)
+//                {
+//                    loadContacts();
+//                }
+//            });
+//            loadContacts();
+//        }
+//
+//        private void loadContacts()
+//        {
+//            listData.clear();
+//
+//            ContentResolver resolver = getContentResolver();
+//
+//            Uri contactsUri = ContactsContract.CommonDataKinds.Phone.CONTENT_URI;  // contacts table uri
+//            //String[] fields = {ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME};
+//            //String sortOreder = ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME + " ASC"; // DESC(DOWN order) / ASC(UP order)
+//            Cursor cursor = resolver.query(contactsUri,null,null,null, null); // SELECT * FROM Contacts
+//
+//            if(cursor != null && cursor.moveToFirst())
+//            {
+//                do
+//                {
+//                    int nameIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
+//                    String name = cursor.getString(nameIndex);
+//
+//                    int phoneIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.NUMBER);
+//                    String num = cursor.getString(phoneIndex);
+//
+//                    Log.d("mylog", name + " " + num);
+//                    listData.add(name + " " + num);
+//                    adapter.notifyDataSetChanged();
+//
+//                } while (cursor.moveToNext());
+//
+//            }
+//            else
+//            {
+//                Toast.makeText(this, "EMPTY CONTACTS LIST", Toast.LENGTH_LONG).show();
+//            }
+//        }
     }
+}
--- app/src/main/java/com/nomyin/dairyapp/MainActivity.java
+++ app/src/main/java/com/nomyin/dairyapp/MainActivity.java
@@ -1,5 +1,6 @@
 package com.nomyin.dairyapp;
 
+import androidx.activity.result.ActivityResultLauncher;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.core.app.ActivityCompat;
@@ -20,6 +21,7 @@ import android.os.Build;
 import android.os.Bundle;
 import android.provider.ContactsContract;
 import android.text.InputType;
+import android.util.Log;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
@@ -57,14 +59,16 @@ public class MainActivity extends AppCompatActivity {
     private Bitmap eventImageBitmap;
     private String eventImageUrl;
     private String eventName ;
-   private String contactName;
+   private String eventContactName;
     private String eventDateStr;
     private String eventNote;
-    String selectedDate;
+    private String selectedDate;
     private NotificationManager notificationManager;
     private int nID = 0;
     private static final int REQUEST_CAMERA_PHOTO = 1;
     private static final int REQUEST_READ_CONTACTS_PERMISSION = 1;
+    private ActivityResultLauncher<Intent> contactPickerLauncher;
+
 
     FirebaseFirestore db = FirebaseFirestore.getInstance();
     @Override
@@ -84,6 +88,7 @@ public class MainActivity extends AppCompatActivity {
         addNewEvent();
 //        setupNotification();
 //        showNotification();
+
    }
     //create the menu
     public boolean onCreateOptionsMenu(Menu menu) {
@@ -115,7 +120,7 @@ public class MainActivity extends AppCompatActivity {
         String strDeviceOS = "Android OS " + Build.VERSION.RELEASE + " API " + Build.VERSION.SDK_INT;
         AlertDialog.Builder dialog = new AlertDialog.Builder(this);
         dialog.setTitle("About App");
-        dialog.setMessage("\nEvent Diary Reminder \n\n By Naomi Inbal-Weinstein \n\n 19.06.2023 \n\n " + strDeviceOS + "\n\n" + "Android 11(R) API Level 30 ");
+        dialog.setMessage("\nEvent Dairy Reminder \n\n By Naomi Inbal-Weinstein \n\n 19.06.2023 \n\n " + strDeviceOS + "\n\n" + "Android 11(R) API Level 30 ");
         dialog.setPositiveButton("OK", (dialog1, which) -> {
             dialog1.dismiss();   // close this dialog
         });
@@ -156,10 +161,15 @@ public class MainActivity extends AppCompatActivity {
     private void showAddEventDialog() {
         AlertDialog.Builder builder = new AlertDialog.Builder(this);
         builder.setTitle("Add New Event");
-
         // Set up the input fields
         LinearLayout layout = new LinearLayout(this);
         layout.setOrientation(LinearLayout.VERTICAL);
+//clear the variables
+        eventName="";
+        eventContactName ="";
+        eventNote="";
+        eventImageUrl="";
+        selectedDate = "";
 
         eventNameInput = new EditText(this);
         eventNameInput.setHint("Enter event name");
@@ -169,15 +179,7 @@ public class MainActivity extends AppCompatActivity {
         chooseContactButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Request permission to read contacts
-                if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS)
-                        != PackageManager.PERMISSION_GRANTED) {
-                    ActivityCompat.requestPermissions(MainActivity.this,
-                            new String[]{Manifest.permission.READ_CONTACTS},
-                            REQUEST_READ_CONTACTS_PERMISSION);
-                } else {
-                    openContactPicker(); // If permission is already granted, open the contact picker
-                }
+           permissionContacts();
             }
         });
 
@@ -187,26 +189,7 @@ public class MainActivity extends AppCompatActivity {
         eventDateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Create a Calendar instance
-                Calendar calendar = Calendar.getInstance();
-
-                // Get the current date values
-                int year = calendar.get(Calendar.YEAR);
-                int month = calendar.get(Calendar.MONTH);
-                int day = calendar.get(Calendar.DAY_OF_MONTH);
-
-                // Create a date picker dialog
-                DatePickerDialog datePickerDialog = new DatePickerDialog(
-                        MainActivity.this,
-                        (view, year1, monthOfYear, dayOfMonth) -> {
-                            // Update the event date input field with the selected date
-                            selectedDate = year1 + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
-                            eventDateButton.setText(selectedDate);
-                        },
-                        year, month, day);
-
-                // Show the date picker dialog
-                datePickerDialog.show();
+             createCalander();
             }
         });
 
@@ -217,7 +200,7 @@ public class MainActivity extends AppCompatActivity {
         eventImageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                // Start the camera activity
+               takePicture(); // Start the camera activity
 
             }
         });
@@ -233,22 +216,7 @@ public class MainActivity extends AppCompatActivity {
         builder.setPositiveButton("Save", new DialogInterface.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
-                eventName = eventNameInput.getText().toString();
-                //TODO initialise the contact that selected
-                contactName ="" ;
-                eventNote = eventNoteInput.getText().toString();
-//convert string to date
-//                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
-//                Date eventDate = null;
-//                try {
-//                    eventDate = sdf.parse(eventDateStr);
-//                } catch (ParseException e) {
-//                    e.printStackTrace();
-//                }
-
-                events.add(new MyEvent(eventName, contactName, selectedDate, eventNote, eventImageUrl));
-
-                eventAdapter.notifyDataSetChanged();
+               saveNewEvent();
             }
         });
 
@@ -268,17 +236,19 @@ public class MainActivity extends AppCompatActivity {
                 openContactPicker(); // Permission granted, open the contact picker
             } else {
                 Toast.makeText(this, "Permission denied.", Toast.LENGTH_SHORT).show();
+                //dialog.setCanceledOnTouchOutside(false);
             }
         }
     }
     private void openContactPicker() {
-        Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
-        startActivityForResult(intent, REQUEST_PICK_CONTACT);
+            Intent intent = new Intent(Intent.ACTION_PICK, ContactsContract.Contacts.CONTENT_URI);
+            startActivityForResult(intent, REQUEST_PICK_CONTACT);
     }
 
     @Override
     protected void onActivityResult(int requestCode, int resultCode, Intent data) {
         super.onActivityResult(requestCode, resultCode, data);
+        //TODO check if permission denied
         if (requestCode == REQUEST_PICK_CONTACT && resultCode == RESULT_OK) {
             if (data != null) {
                 Uri contactUri = data.getData();
@@ -287,14 +257,67 @@ public class MainActivity extends AppCompatActivity {
                 try (Cursor cursor = getContentResolver().query(contactUri, projection, null, null, null)) {
                     if (cursor != null && cursor.moveToFirst()) {
                         int nameIndex = cursor.getColumnIndex(ContactsContract.CommonDataKinds.Phone.DISPLAY_NAME);
-                        String contactName = cursor.getString(nameIndex);
-                        // Set the selected contact name to the variable
-                        // You can update the UI or use it in further
+                        eventContactName = cursor.getString(nameIndex);
+                        chooseContactButton.setText(eventContactName);
 
                     }
                 }
             }
         }}
+    private void permissionContacts(){
+        // Request permission to read contacts
+        if (ContextCompat.checkSelfPermission(MainActivity.this, Manifest.permission.READ_CONTACTS)
+                != PackageManager.PERMISSION_GRANTED) {
+
+            ActivityCompat.requestPermissions(MainActivity.this,
+                    new String[]{Manifest.permission.READ_CONTACTS},
+                    REQUEST_READ_CONTACTS_PERMISSION);
+        } else {
+            openContactPicker(); // If permission is already granted, open the contact picker
+        }
+    }
+    private void createCalander(){
+        // Create a Calendar instance
+        Calendar calendar = Calendar.getInstance();
+
+        // Get the current date values
+        int year = calendar.get(Calendar.YEAR);
+        int month = calendar.get(Calendar.MONTH);
+        int day = calendar.get(Calendar.DAY_OF_MONTH);
+
+        // Create a date picker dialog
+        DatePickerDialog datePickerDialog = new DatePickerDialog(
+                MainActivity.this,
+                (view, year1, monthOfYear, dayOfMonth) -> {
+                    // Update the event date input field with the selected date
+                    selectedDate = year1 + "-" + (monthOfYear + 1) + "-" + dayOfMonth;
+                    eventDateButton.setText(selectedDate);
+                },
+                year, month, day);
+
+        // Show the date picker dialog
+        datePickerDialog.show();
+    }
+    private void saveNewEvent(){
+        //TODO required fields
+             //All variables have values, proceed with saving
+            eventName = eventNameInput.getText().toString();
+            eventNote = eventNoteInput.getText().toString();
+            eventDateStr = selectedDate;
+            events.add(new MyEvent(eventName, eventContactName, eventDateStr, eventNote, eventImageUrl));
+            eventAdapter.notifyDataSetChanged();
+       //convert string to date
+//                SimpleDateFormat sdf = new SimpleDateFormat("yyyy-MM-dd", Locale.getDefault());
+//                Date eventDate = null;
+//                try {
+//                    eventDate = sdf.parse(eventDateStr);
+//                } catch (ParseException e) {
+//                    e.printStackTrace();
+//                }
+    }
+    private void takePicture(){
+
+    }
                     ////TODO put in a thread
 //    //firebase - store the data
 //    // Create a new user with a first and last name
